9dddec9cc053a3310511b118a9050b361311301b
Codi-K <codikader7@gmail.com>
2023-05-16 14:05:43 +0200

    exercise

--- script/code.js
+++ script/code.js
@@ -1,7 +1,7 @@
 let subject = ["Python", "C++", "VB.Net", "Javascript", "Assembly"];
 console.log(subject);
 
-let last = subject.pop()
+let last = subject.pop(4)
 console.log(last)
 
-subject[2]
+console.log(subject[2])
